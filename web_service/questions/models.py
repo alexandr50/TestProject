fb46314110c569b06556da62478761d90a27abad
alexandr <dmitrymaslov2016@mai.ru>
2023-10-16 22:00:30 +0300

    create serializer and view

--- web_service/questions/models.py
+++ web_service/questions/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
 class Question(models.Model):
-    id = models.IntegerField(unique=True, primary_key=True)
+    id_for_response = models.BigAutoField(primary_key=True, verbose_name='id с сайта')
+    id = models.PositiveIntegerField(unique=True)
     answer = models.CharField(max_length=255, verbose_name='ответ')
     question = models.TextField(verbose_name='вопрос')
     created_at = models.DateTimeField(verbose_name='дата создания', null=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
--- web_service/questions/views.py
+++ web_service/questions/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import requests
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionsView(generics.ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def post(self, request, *args, **kwargs):
+        queryset_ids = [x.id_for_response for x in Question.objects.all()]
+        response = requests.get('https://jservice.io/api/random?count=1 ')
+        print(response.json)
+        return Response(response.json())
